d1f2943ff872a0df3105434985e561e030401f99
Kenjii <kenjii@0xmakase.co.jp>
2024-06-28 10:53:03 +0900

    スクリプトの挙動確認後、エラー部分修正

--- docker-compose/generate-configs.sh
+++ docker-compose/generate-configs.sh
@@ -9,20 +9,20 @@ UPPER_CHAIN_NAME=$(echo "$CHAIN_NAME" | tr 'a-z' 'A-Z')
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 COMMON_BLOCKSCOUT_ENV_FILE="envs/common-blockscout.env"
 COMMON_FRONTEND_ENV_FILE="envs/common-frontend.env"
-USER_OPS_INDEXER_FILE="user-ops-indexer.yml"
+USER_OPS_INDEXER_FILE="services/user-ops-indexer.yml"
 
 # Replace placeholders in the docker-compose.yml
-sed -i "s|{{RPC_URL}}|$RPC_URL|g" $DOCKER_COMPOSE_FILE
-sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" $DOCKER_COMPOSE_FILE
+sed -i "s|{{RPC_URL}}|$RPC_URL|g" "$DOCKER_COMPOSE_FILE"
+sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" "$DOCKER_COMPOSE_FILE"
 
 # Replace placeholders in the common-blockscout.env
-sed -i "s|{{RPC_URL}}|$RPC_URL|g" $COMMON_BLOCKSCOUT_ENV_FILE
+sed -i "s|{{RPC_URL}}|$RPC_URL|g" "$COMMON_BLOCKSCOUT_ENV_FILE"
 
 # Replace placeholders in the common-frontend.env
-sed -i "s|{{CURRENCY_NAME}}|$CHAIN_NAME|g" $COMMON_FRONTEND_ENV_FILE
-sed -i "s|{{CURRENCY_SYMBOL}}|$UPPER_CHAIN_NAME|g" $COMMON_FRONTEND_ENV_FILE
+sed -i "s|{{CURRENCY_NAME}}|$CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
+sed -i "s|{{CURRENCY_SYMBOL}}|$UPPER_CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
 
 # Replace placeholders in the user-ops-indexer.yml
-sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" $USER_OPS_INDEXER_FILE
+sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" "$USER_OPS_INDEXER_FILE"
 
 echo "Configuration files generated for $CHAIN_NAME"
