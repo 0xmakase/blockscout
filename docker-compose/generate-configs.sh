ee4bf924ca185dff08c8c3ca603976f9102fdaf2
Kenjii <kenjii@0xmakase.co.jp>
2024-07-03 21:25:31 +0900

    Linuxで実行できるように修正

--- docker-compose/generate-configs.sh
+++ docker-compose/generate-configs.sh
@@ -14,22 +14,22 @@ USER_OPS_INDEXER_FILE="services/user-ops-indexer.yml"
 DEFAULT_CONF_TEMPLATE_FILE="proxy/default.conf.template"
 
 # Replace placeholders in the docker-compose.yml
-sed -i '' "s|{{RPC_URL}}|$RPC_URL|g" "$DOCKER_COMPOSE_FILE"
-sed -i '' "s|{{RPC_WS_URL}}|$WS_URL|g" "$DOCKER_COMPOSE_FILE"
-sed -i '' "s|{{CHAIN_ID}}|$CHAIN_ID|g" "$DOCKER_COMPOSE_FILE"
+sed -i "s|{{RPC_URL}}|$RPC_URL|g" "$DOCKER_COMPOSE_FILE"
+sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" "$DOCKER_COMPOSE_FILE"
+sed -i "s|{{CHAIN_ID}}|$CHAIN_ID|g" "$DOCKER_COMPOSE_FILE"
 
 # Replace placeholders in the common-blockscout.env
-sed -i '' "s|{{RPC_URL}}|$RPC_URL|g" "$COMMON_BLOCKSCOUT_ENV_FILE"
+sed -i "s|{{RPC_URL}}|$RPC_URL|g" "$COMMON_BLOCKSCOUT_ENV_FILE"
 
 # Replace placeholders in the common-frontend.env
-sed -i '' "s|{{CURRENCY_NAME}}|$CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
-sed -i '' "s|{{CURRENCY_SYMBOL}}|$UPPER_CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
-sed -i '' "s|{{DOMAIN_NAME}}|$DOMAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
+sed -i "s|{{CURRENCY_NAME}}|$CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
+sed -i "s|{{CURRENCY_SYMBOL}}|$UPPER_CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
+sed -i "s|{{DOMAIN_NAME}}|$DOMAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
 
 # Replace placeholders in the user-ops-indexer.yml
-sed -i '' "s|{{RPC_WS_URL}}|$WS_URL|g" "$USER_OPS_INDEXER_FILE"
+sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" "$USER_OPS_INDEXER_FILE"
 
 # Replace placeholders in the default.conf.template
-sed -i '' "s|{{DOMAIN_NAME}}|$DOMAIN_NAME|g" "$DEFAULT_CONF_TEMPLATE_FILE"
+sed -i "s|{{DOMAIN_NAME}}|$DOMAIN_NAME|g" "$DEFAULT_CONF_TEMPLATE_FILE"
 
 echo "Configuration files generated for $CHAIN_NAME"
