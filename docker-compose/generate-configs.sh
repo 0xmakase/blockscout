3acf02d5cd72831af076af7cf8c9f5039cda618b
Kenjii <kenjii@0xmakase.co.jp>
2024-07-22 09:25:30 +0900

    フロントエンドのChainID書き換え追加

--- docker-compose/generate-configs.sh
+++ docker-compose/generate-configs.sh
@@ -25,6 +25,7 @@ sed -i "s|{{RPC_URL}}|$RPC_URL|g" "$COMMON_BLOCKSCOUT_ENV_FILE"
 sed -i "s|{{CURRENCY_NAME}}|$CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
 sed -i "s|{{CURRENCY_SYMBOL}}|$UPPER_CHAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
 sed -i "s|{{DOMAIN_NAME}}|$DOMAIN_NAME|g" "$COMMON_FRONTEND_ENV_FILE"
+sed -i "s|{{CHAIN_ID}}|$CHAIN_ID|g" "$COMMON_FRONTEND_ENV_FILE"
 
 # Replace placeholders in the user-ops-indexer.yml
 sed -i "s|{{RPC_WS_URL}}|$WS_URL|g" "$USER_OPS_INDEXER_FILE"
